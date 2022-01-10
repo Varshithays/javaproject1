ff5817e2ec3640304e3f2f4fcb4bf8f291b19cd8
Varshithays <86124941+Varshithays@users.noreply.github.com>
2022-01-11 02:08:53 +0530

    Update and rename mortgagecalci to mortgagecalci.java

---
+++ mortgagecalci.java
@@ -0,0 +1,29 @@
+package com.javavarshitha;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final byte monthsInYear =12;
+        final byte  percent = 100;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Principal: ");
+        int principal = scanner.nextInt();
+        System.out.print("Annual Interest Rate: ");
+        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest/percent/monthsInYear;
+        System.out.print("period(Years): ");
+
+        byte years = scanner.nextByte();//mortgage formula
+        int numberOfPayments = years * monthsInYear;// mf = P(1+r)^n/(1+r)^n-1
+
+        double mortgage = principal*(monthlyInterest * Math.pow(1+monthlyInterest,numberOfPayments))/
+                (Math.pow(1+monthlyInterest,numberOfPayments))-1;
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
+    }
+}
